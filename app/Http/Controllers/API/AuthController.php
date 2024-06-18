e6f5cbd3dbd86bf6716fcc696572044e15c87ed4
Amirul Abd Latib <amirul.latib@alphv.com>
2024-06-18 16:51:12 +0800

    Protect api route with sanctum

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.'
+            ], 401);
+        }
+
+        $token = $user->createToken('token-name')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\MenuController;
 use App\Http\Controllers\API\SaleController;
 use App\Http\Controllers\API\UserController;
@@ -24,10 +25,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('users',UserController::class);
-Route::apiResource('categories',CategoryController::class);
-Route::apiResource('sales',SaleController::class);
-Route::apiResource('orders',OrderController::class);
-Route::apiResource('menus',MenuController::class);
-Route::apiResource('loyalty-points', LoyaltyPointsController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('sales', SaleController::class);
+    Route::apiResource('orders', OrderController::class);
+    Route::apiResource('menus', MenuController::class);
+    Route::apiResource('loyalty-points', LoyaltyPointsController::class);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
